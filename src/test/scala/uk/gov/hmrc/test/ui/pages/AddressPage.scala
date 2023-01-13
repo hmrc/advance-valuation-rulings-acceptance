5380c9130482e5b6ceb312c95ed802910dd4f441
sitaramireddytanuboddi <119498640+sitaramireddytanuboddi@users.noreply.github.com>
2023-01-13 16:41:59 +0000

    updated scripts

--- src/test/scala/uk/gov/hmrc/test/ui/pages/AddressPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/AddressPage.scala
@@ -19,10 +19,8 @@ package uk.gov.hmrc.test.ui.pages
 object AddressPage extends BasePage {
 
   val pageTitle                 = "Check the name and address for EORI number - Advance Ruling Service - GOV.UK"
-  val errorPageTitle            = {
+  val errorPageTitle            =
     "Error: Check the name and address for EORI number - Advance Ruling Service - GOV.UK"
-  }
-
   def loadPage: this.type       = {
     onPage(this.pageTitle)
     this
